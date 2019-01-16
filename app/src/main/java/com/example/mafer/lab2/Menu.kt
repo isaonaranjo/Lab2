ecfff50de1948ec707fc274003e84e4a11434435
diazMafer <31084398+diazMafer@users.noreply.github.com>
2019-01-16 09:06:26 -0600

    Initial commit

---
+++ app/src/main/java/com/example/mafer/lab2/Menu.kt
@@ -0,0 +1,19 @@
+package com.example.mafer.lab2
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.ArrayAdapter
+import kotlinx.android.synthetic.main.activity_menu.*
+
+
+class Menu : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_menu)
+
+        //
+        //list_view_products.adapter
+
+    }
+}
